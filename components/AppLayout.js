c09aab4ff98a45e0f59447c3435d0b39111a931b
choi0926 <ghtjd753753@gmail.com>
2022-06-16 01:39:33 +0900

    Add eslint setting

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import Link from 'next/link';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>메뉴</div>
+      <div>
+        <Link href="/">
+          <a>홈</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
