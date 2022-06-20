547c71d61b84a02af46596ecc956e8f857f7c318
choi0926 <ghtjd753753@gmail.com>
2022-06-20 17:10:08 +0900

    Add head

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import 'antd/dist/antd.css';
+import Head from 'next/head';
 
 const App = ({ Component }) => {
-  return <Component></Component>;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8"></meta>
+        <title>todays-workout</title>
+      </Head>
+      <Component></Component>
+    </>
+  );
 };
 
 App.propTypes = {
