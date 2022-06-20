5e20de507977c66971a05236f9a8fdd057ac79c1
choi0926 <ghtjd753753@gmail.com>
2022-06-20 17:56:16 +0900

    Add grid

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
@@ -13,11 +13,25 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          오른쪽
+        </Col>
+      </Row>
       {children}
     </div>
   );
